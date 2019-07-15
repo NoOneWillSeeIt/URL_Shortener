ee8bd78d17c47294982bf123c09719f2e81e4a78
NoOneWillSeeIt <rest338@gmail.com>
2019-07-15 19:09:48 +0700

    huge front-end improvements and few back-end fixes

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -18,28 +18,44 @@ def url_validate(url):
 
 @app.route('/process', methods=['POST'])
 def process():
-    url = request.form['url']
-    if not url_validate(url):
-        return jsonify({'error': 'incorrect url'})
-    hours = request.form['hours']
+    url = ''
+    hours = 0
+    try:
+        url = request.form['url']
+        if not url_validate(url):
+            return jsonify({'error': 'incorrect url'})
+        hours = request.form['hours']
+    except:
+        return jsonify({'error': 'incorrect request'}), 500
+
     storage_time = timedelta(hours=int(hours))
     estimated_date = datetime.utcnow() + storage_time
-    shortened = token_urlsafe(4)
-    url_obj = URL(url=url, shortened=shortened, estimated_date=estimated_date)
-    try:
-        db.session.add(url_obj)
-        db.session.commit()
-    except:
-        return jsonify({'error': 'internal server error'}), 500
+    
+    shortened = ''
+    tries = 0
+    while(True):
+        try:
+            shortened = token_urlsafe(4)
+            url_obj = URL(url=url, shortened=shortened, estimated_date=estimated_date)
+            db.session.add(url_obj)
+            db.session.commit()
+            break
+        except:
+            tries += 1
+            if tries > 4:
+                return jsonify({'error': 'internal server error'}), 500
+
     return jsonify({'shortened': shortened, 'estimated_date': estimated_date.strftime('%d.%m.%Y %H:%M:%S')})
 
 
 @app.route('/<string:shortened>')
 def return_redirect(shortened):
     url = URL.query.filter_by(shortened=shortened).first()
-    print(url)
     if url:
-        return redirect(url.url, code=302)
-    else:
-        flash('This link is invalid or expired!', 'danger')
+        if datetime.utcnow() < url.estimated_date:
+            return redirect(url.url, code=302)
+        else:
+            db.session.delete(url)
+            db.session.commit()
+    flash('This link is invalid or expired!', 'danger')
     return redirect(url_for('home'))
--- url_shortener/static/js/ajax.js
+++ url_shortener/static/js/ajax.js
@@ -1,26 +1,53 @@
+let range = document.getElementById('form_range');
+let label = document.getElementById('label_form_range');
 
 let shorten_btn = document.getElementById('shorten_btn');
-let copy_btn = document.getElementById('copy_btn');
+let old_url_input_value = '';
+let old_range_value = 0;
+
+let duration = [
+	{label: '24 Hours', hours: 24},
+	{label: '1 Week', hours: 168},
+	{label: '2 Weeks', hours: 336},
+	{label: '1 Month', hours: 720},
+	{label: '2 Months', hours: 1440}
+]
 
 shorten_btn.onclick = function() {
 	url_link = document.getElementById('url_input').value;
 	if (!url_link) {
+		let report_div = document.getElementById('report');
+		report_div.innerHTML = 'Paste any link before shorten :)';
+		report_div.classList.remove('invisible');
 		return;
 	}
+
+	if (old_url_input_value == url_link && range.value == old_range_value) {
+		let report_div = document.getElementById('report');
+		report_div.innerHTML = 'Paste new link before shorten :)';
+		report_div.classList.remove('invisible');
+		return;
+	}
+
+	document.getElementById('report').classList.add('invisible');
+	old_url_input_value = url_link;
+	old_range_value = range.value;
+	
 	let xhr = new XMLHttpRequest();
 	xhr.open('POST', 'process', true);
 
 	let body = new FormData();
 	body.append('url', url_link);
-	body.append('hours', '24');
-	
+	let hours = duration[range.value]['hours'];
+	body.append('hours', hours.toString());
+
 	xhr.onload = function() {
 		if (xhr.status == 200 && xhr.readyState == 4) {
 			let json_response = JSON.parse(xhr.responseText);
 			let url_shortened = document.getElementById('url_shortened');
 			let link = window.location.protocol + '//' + window.location.host + '/' + json_response.shortened;
 			url_shortened.innerHTML = link;
-			document.getElementById('url_container').style.display = 'block';
+			document.getElementById('url_container').classList.remove('invisible');
 		}
 		else {
 			let report_div = document.getElementById('report');
@@ -28,18 +55,31 @@ shorten_btn.onclick = function() {
 			if (xhr.responseText) {
 				let json_response = JSON.parse(xhr.responseText)['error'];
 			}
-			report_div.setText('error ' + json_response);
-			report_div.style.display = 'block';
+			report_div.innerHTML = 'error: ' + json_response;
+			report_div.classList.remove('invisible');
 		}
 	};
 	xhr.send(body);
 };
 
+let copy_btn = document.getElementById('copy_btn');
+
 copy_btn.onclick = function() {
-	let range = document.createRange();
-    range.selectNode(document.getElementById('url_shortened'));
+	let selection_range = document.createRange();
+    selection_range.selectNode(document.getElementById('url_shortened'));
     let sel = window.getSelection();
     sel.removeAllRanges();
-    sel.addRange(range);
-    document.execCommand("copy");
+    sel.addRange(selection_range);
+    document.execCommand('copy');
 }
+
+
+function updateRangeInput() {
+	label.innerHTML = duration[range.value]['label'];
+}
+
+range.addEventListener('change', updateRangeInput);
+range.addEventListener('input', updateRangeInput);
+
+range.value = 0;
+updateRangeInput();
